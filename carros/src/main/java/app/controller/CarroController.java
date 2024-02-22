1ed85b6cbef3a28d77f2cef82d2c92916040d095
Wellington <Wellington@DESKTOP-PQBMPPJ>
2024-02-22 11:10:22 -0300

    dsafds

--- carros/src/main/java/app/controller/CarroController.java
+++ carros/src/main/java/app/controller/CarroController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -85,7 +86,7 @@ public class CarroController {
 		
 	}
 	
-	@GetMapping("/delete/{idCarro}")
+	@DeleteMapping("/delete/{idCarro}")
 	public ResponseEntity<String> delete(@PathVariable long idCarro){
 		
 		try {
